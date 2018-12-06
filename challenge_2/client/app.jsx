0f019d9078d96c48d14322116e84e7562ac6cd96
Kameron Seto <tan@Sad-Panda.local>
2018-12-06 12:27:48 -0800

    code skeleton

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,2 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,14 @@
+const express = require('express');
 
+const PORT = 3000;
+const app = express();
 
+app.use('/', express.static(`${__dirname}/public`));
+
+app.get('/', (req, res) => {
+
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
