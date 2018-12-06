59ad7c7e789767a4802d271229351492244e1e3c
Kameron Seto <tan@Sad-Panda.local>
2018-12-06 14:22:30 -0800

    basic working app

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,19 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+import { Bar } from 'react-chartjs-2';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      data: {
+        labels: [],
+        datasets: [{
+          label: "",
+          data: [],
+          backgroundColor: 'white',
+          borderColor: 'black',
+          // backgroundColor: 'rgb(255, 99, 132)',
+          // borderColor: 'rgb(255, 99, 132)',
+        }]
+      },
+    };
   }
 
   componentDidMount() {
+    axios.get('/bpi')
+      .then((res) => {
+        const newData = {
+          labels: Object.keys(res.data.bpi),
+          datasets: [{
+            label: "Historical BPI",
+            data: Object.values(res.data.bpi),
+            backgroundColor: 'black',
+            borderColor: 'black',
+          }],
+        };
 
+        this.setState({
+          data: newData,
+        });
+      })
   }
 
   render() {
     return (
       <div>
-
+        <Bar data={this.state.data} />
+        <div>Powered by CoinDesk</div>
       </div>
     );
   }
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const path = require('path');
+const axios = require('axios');
 
 const PORT = 3000;
 const app = express();
 
-app.use('/', express.static(`${__dirname}/public`));
+app.use('/', express.static(path.join(__dirname, '/public')));
 
-app.get('/', (req, res) => {
-
+app.get('/bpi', (req, res) => {
+  axios.get('https://api.coindesk.com/v1/bpi/historical/close.json?start=2013-09-01&end=2013-09-05')
+    .then((response) => {
+      res.send(response.data);
+    });
 });
 
 app.listen(PORT, () => {
