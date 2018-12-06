91bf2a7005e9af242f3d99af699c5321317dc7f7
Kameron Seto <tan@Sad-Panda.local>
2018-12-06 14:47:30 -0800

    line graph with more date options

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-import { Bar } from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
+
+const formatDate = (date) => {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
+};
+
+const msInDay = 24 * 60 * 60 * 1000;
 
 class App extends React.Component {
   constructor(props) {
@@ -13,24 +22,26 @@ class App extends React.Component {
         datasets: [{
           label: "",
           data: [],
-          backgroundColor: 'white',
           borderColor: 'black',
           // backgroundColor: 'rgb(255, 99, 132)',
           // borderColor: 'rgb(255, 99, 132)',
         }]
       },
+      start: formatDate(new Date(new Date() - 30 * msInDay)),
+      end: formatDate(new Date()),
     };
   }
 
   componentDidMount() {
-    axios.get('/bpi')
+    const { start, end } = this.state;
+
+    axios.get(`/bpi?start=${start}&end=${end}`)
       .then((res) => {
         const newData = {
           labels: Object.keys(res.data.bpi),
           datasets: [{
             label: "Historical BPI",
             data: Object.values(res.data.bpi),
-            backgroundColor: 'black',
             borderColor: 'black',
           }],
         };
@@ -44,7 +55,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Bar data={this.state.data} />
+        <Line data={this.state.data} />
         <div>Powered by CoinDesk</div>
       </div>
     );
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -8,7 +8,8 @@ const app = express();
 app.use('/', express.static(path.join(__dirname, '/public')));
 
 app.get('/bpi', (req, res) => {
-  axios.get('https://api.coindesk.com/v1/bpi/historical/close.json?start=2013-09-01&end=2013-09-05')
+  const { start, end } = req.query;
+  axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${start}&end=${end}`)
     .then((response) => {
       res.send(response.data);
     });
